636716282e7d59d4231c5bd465f4ba5beca6b1d2
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-07-22 12:12:22 +0200

    Fixed the testing environment.

--- autoload.php
+++ autoload.php
@@ -10,8 +10,10 @@ require_once __DIR__.'/vendor/autoload.php';
 use Dotenv\Dotenv;
 
 /* Load environment variables */
-if (!array_key_exists('TRAVIS_TEST', $_ENV)) {
+try {
     $dotenv = Dotenv::create(__DIR__);
     $dotenv->load();
+} catch (\Throwable $e) {
+    echo "No environment files (.env) found.";
 }
 
