1c670d5aa28c04519ceeb82cc3f1b68308649861
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2019-07-23 15:48:53 +0200

    Moved utiltiy class.

---
+++ src/Util/Util.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Utility class.
+ * A collection of utility methods.
+ */
+
+namespace BCHDB\Util;
+
+class Util {
+
+    /**
+     * Generate random ID.
+     * Return a random numeric ID of a specified length.
+     * @param int $length Numeric string length.
+     * @return string Random numeric ID.
+     */
+    public static function random_id($length = 6) {
+        $keyspace = '0123456789';
+        $pieces = [ ];
+        $max = mb_strlen($keyspace, '8bit') - 1;
+        for ($i = 0; $i < $length; ++$i) {
+            $pieces []= $keyspace[random_int(0, $max)];
+        }
+        return implode('', $pieces);
+    }
+
+    /**
+     * Convert object to array.
+     * Turn a given object into an array using JSON encoding and decoding.
+     * @param object $object Object to be converted.
+     * @return array Output array.
+     */
+    public static function to_array($object) {
+        $a = json_encode($object);
+        return json_decode(json_encode($object), true);
+    }
+
+    /**
+     * Resursive array filtering.
+     * Recursively filter multi-level and associative arrays to remove empty entries.
+     * @param array $array Array to be filtered.
+     * @return array Filtered array. 
+     */
+    public static function array_filter_recursive(&$array) {
+        foreach ($array as $key => $item) {
+            is_array($item) && !empty($item) && $array[$key] = self::array_filter_recursive($item);
+            if (is_array($array) && $array[$key] === NULL) {
+                unset($array[$key]);
+            }
+        }
+        return $array;
+    } 
+}
